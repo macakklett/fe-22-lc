2fdc56c64b71f34985742f5298598d73cb6c4595
macakklett <macanvitalijj@gmail.com>
2024-07-09 12:23:29 +0300

    fix

--- 09-07_lc/task1/student.js
+++ 09-07_lc/task1/student.js
@@ -17,3 +17,11 @@ export class Student extends User {
     this.#isActive = value;
   }
 }
+
+const student1 = new Student(1, 'Tom', 'Doe', 'FE', 100);
+const student2 = new Student(2, 'Ivan', 'Shev', 'Shvd', 190);
+const student3 = new Student(3, 'Alice', 'Johnson', 'Math', 85);
+
+console.log(student1.getFullName());
+console.log(student2.getFullName());
+console.log(student3.getFullName());
