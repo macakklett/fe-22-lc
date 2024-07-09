1d1c9361a28339be591b60d6703d84563a6d5c43
macakklett <macanvitalijj@gmail.com>
2024-07-09 12:29:42 +0300

    fix

--- 09-07_lc/task1/index.js
+++ 09-07_lc/task1/index.js
@@ -22,6 +22,16 @@ const getTopStudent = students => {
   return { id, firstName, lastName };
 };
 
+const user1 = new User(1, 'John', 'Doe');
+const user2 = new User(2, 'Jane', 'Doe');
+const user3 = new User(3, 'Jim', 'Beam');
+const user4 = new User(4, 'Tom', 'Holl');
+
+console.log(user1.getFullName());
+console.log(user2.getFullName());
+console.log(user3.getFullName());
+console.log(user4.getFullName());
+
 // testing
 const student1 = new Student(1, 'Tom', 'Doe', 'FE', 100);
 const student2 = new Student(2, 'Ivan', 'Shev', 'Shvd', 190);
